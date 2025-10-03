66dc4e4aaef2f1b79c409cd0b453d53c5f3291f7
Landon Johnson <landon@hellojohnsons.com>
2025-10-03 14:02:42 +0000

    Added Stuff

--- html.php
+++ html.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
@@ -11,6 +11,41 @@
         <h4>CONGRATS!</h4>
         <div class="boi">
             <p style="color:red">ALSO IF YOU"RE READING THIS YOU"RE FAT.</div>
+            <a href="https://www.youtube.com/@ApocalypticEditzz15" target="_blank">Thingy</a>
+            <p style="color:green">
+            <form method="POST">
+                <p>
+                    <label for="email">Email</label>
+                    <input name="email" typle="email" placeholder="Your email here. HEHEHEHE" />
+                </p>
+                <p>
+                    <label for="gender">Gender</label>
+                    <select name="gender">
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="rather not Say">Rather Not Say</option>
+                        <option value="croissant">Croissant</option>
+</select>
+                </p>
+                <p>
+                    <input type="submit" value="SEND" />
+</p>
+            </form>
+            
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Hobby</th>
+                </tr>
+            </thead>
+            </tbody>
+            <tr>
+                <td>Landon</td>
+                <td>Edit</td>
+            </tr>
+            <a href="#big boi">This is thingy!</a>
         </div>
     </body>
 </html>
